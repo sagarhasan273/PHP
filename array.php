8af2281d4c7d83e1a4107659a7eb547d1388ea2f
sagarhasan273 <sagarhasan273@gmail.com>
2022-05-07 20:28:23 +0600

    Associative Array

--- array.php
+++ array.php
@@ -2,3 +2,22 @@
 $car = array("BMW", "TOYOTA", "VOLVO", "FORD");
 
 echo "I like $car[0], $car[1], $car[2] and $car[3]";
+
+foreach ($car as $cr){
+    echo $cr."\n";
+}
+
+// ----------------Associative Array------------
+$fruits = array();
+$fruits["summer"] = "mangos";
+$fruits["winter"] = "apples";
+$fruits["spring"] = "oranges";
+$fruits[3] = "kiwi";
+
+
+foreach ($fruits as $fts => $ft){
+    echo $fts." => $ft\n";
+    echo $fruits[$fts];
+    echo "\n";
+}
+
