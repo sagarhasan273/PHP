b648542f06105ca21e2fce9ad0290affec3ae394
sagarhasan273 <sagarhasan273@gmail.com>
2022-05-07 20:31:49 +0600

    Multidimensional Arrays

--- array.php
+++ array.php
@@ -21,3 +21,18 @@ foreach ($fruits as $fts => $ft){
     echo "\n";
 }
 
+$cars = array (
+    array("Volvo",22,18),
+    array("BMW",15,13),
+    array("Saab",5,2),
+    array("Land Rover",17,15)
+  );
+for ($row = 0; $row < 4; $row++) {
+    echo "Row number $row";
+    echo "\n";
+    for ($col = 0; $col < 3; $col++) {
+      echo "".$cars[$row][$col]."\n";
+    }
+    echo "\n";
+  }
+  
