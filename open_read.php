6774b05c11620ccbcbd68bf933f7c53c10a6508c
sagarhasan273 <sagarhasan273@gmail.com>
2022-05-08 22:29:31 +0600

    open_read.php

---
+++ open_read.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$myfile = fopen("webdictionary.txt", "r") or die('<script>alert("Welcome to Geeks for Geeks")</script>');
+echo fread($myfile,filesize("webdictionary.txt"));
+fclose($myfile);
+?>
+</body>
+</html>
