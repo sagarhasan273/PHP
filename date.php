146a472ce7c66d3f5210dc3d0bf679d4ab18977f
sagarhasan273 <sagarhasan273@gmail.com>
2022-05-08 22:20:12 +0600

    readfile

--- date.php
+++ date.php
@@ -3,3 +3,8 @@
 date_default_timezone_set("asia/Dhaka");
 echo "The time is " . date("h:i:sa");
 ?>
+
+<!-- -------------file read-------------->
+<?php
+echo readfile("C:\Users\sagar\Desktop\Jobs requirements.txt");
+?>
